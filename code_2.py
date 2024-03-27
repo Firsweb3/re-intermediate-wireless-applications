6d71fb0cf113f5a67286ee50c30c447920cd1ff2
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2024-03-28 01:26:06 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    });
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    for (let i = 0; i < 10; i++) {
+}
+    const data = [];
+
+    }
+    data.forEach(item => {
+
+    return data;
+function generateRandomData() {
+}
+
+function main() {
