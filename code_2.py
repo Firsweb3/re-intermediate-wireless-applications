3b27ee7a291b0d3c3a82dc2c731855a4939b0447
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2024-03-29 07:05:00 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,61 +1,17 @@
+        console.log("Random Number: " + item);
 
-        int[] data = new int[10];
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
 
-        }
-        for (int i = 0; i < 10; i++) {
-    }
 }
-            System.out.println("Random Number: " + item);
-        }
-        for (int item : data) {
-public class RandomDataGenerator {def generate_random_data():
-
-
-
-import random
-
-    return data
-def main():
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    data = generate_random_data()
-    main()public class RandomDataGenerator {
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-
-    }
-        }
-        int[] data = new int[10];
+    const data = [];
 }
-        for (int item : data) {
-        }        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    data.forEach(item => {
+    return data;
+    const data = generateRandomData();
+main();
 
+function main() {
+    for (let i = 0; i < 10; i++) {
     }
-
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-        }
-        for (int item : data) {
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
-        }
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-        for (int item : data) {
-        int[] data = new int[10];
-        }
-        for (int i = 0; i < 10; i++) {
-}
-
+function generateRandomData() {
