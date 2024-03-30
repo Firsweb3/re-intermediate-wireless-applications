7b832be0afd33a9b4425a472ea681e4cd9571fa3
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2024-03-30 20:33:59 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,13 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
 
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-}
     }
         }
-        }
-
-    public static void main(String[] args) {
-        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
 public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+            System.out.println("Random Number: " + item);
+}
+        int[] data = new int[10];
+        for (int item : data) {
